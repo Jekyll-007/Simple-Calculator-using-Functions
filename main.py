feffca57ff85d6c14fc8664d3a6cc62de0b1758d
Jekyll-007 <teenofeejay@gmail.com>
2024-10-18 17:51:43 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def add(x,y):
+    return x+y
+def multiply(x,y):
+    return x*y
+def subtract(x,y):
+    return x-y
+def divide(x,y):
+    return x/y
+numb1=int(input("Enter first number:"))
+numb2=int(input("Enter second number:"))
+print("The sum is: ",add(numb1,numb2))
+print("The product is:",multiply(numb1,numb2))
+print("The difference is:",subtract(numb1,numb2))
+print("The quotient is:",divide(numb1,numb2))
